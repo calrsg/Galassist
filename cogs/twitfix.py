9e9b14886cf55d785f50d976f82d9edaf4cd0f92
Callum Sproul <sprcr001@mymail.unisa.edu.au>
2023-11-17 16:42:18 +1030

    Support for spoiler messages, reminder if fixed tweet receives a reply, can be toggled on/off

--- cogs/twitfix.py
+++ cogs/twitfix.py
@@ -14,40 +14,64 @@ class TwitFix(commands.Cog):
         self.bot.loop.create_task(self.init_log())
 
     async def init_log(self):
-        await self.log.load()  # Load the data asynchronously
+        await self.log.load()
 
     @commands.Cog.listener()
     async def on_message(self, message):
-        replace = ["twitter.com", "x.com"]
-        ignore = ["fxtwitter.com", "vxtwitter.com"]
+        # Ignore if function turned off or message author is a bot
         if message.author.bot or not self.status:
             return
+
+        # Intuitive replies
+        if message.reference and message.reference.resolved:
+            parent = message.reference.resolved
+            if parent.author == self.bot.user:
+                parent_ref = await message.channel.fetch_message(parent.reference.message_id)
+                if parent.reference and parent_ref:
+                    parent_author = parent_ref.author
+                    if parent_author == message.author:
+                        None
+                    else:
+                        if await self.log.get_ignored(parent_author.id) is not True:
+                            await parent_author.send(f"{message.author.display_name} replied to a fixed Tweet you posted in {message.guild.name}: "
+                                                     f"https://discord.com/channels/{message.guild.id}/{message.channel.id}/{message.id}\n"
+                                                     f"If you want to disable these reminders, use the command /tfremind.")
+
+        # URL Replacements
+        replace = ["twitter.com", "x.com"]
+        ignore = ["fxtwitter.com", "vxtwitter.com"]
+
+        # Ignore if message contains pre-fixed URLs
         for i in ignore:
             if i in message.content:
                 return
+        # Check if URLs contain replacements before Regex searching
         count = 0
         for r in replace:
             if r in message.content:
-                count += 1
-        if count > 0:
-            new_content = ""
-            url_regex = r"(https?:\/\/)(www\.)?(twitter\.com|x\.com)(\/[-a-zA-Z0-9()@:%_\+.~#?&//=]*)?"
+                new_content = ""
+                url_regex = r"(https?:\/\/)(www\.)?(twitter\.com|x\.com)(\/[-a-zA-Z0-9()@:%_\+.~#?&//=]*)?"
+                urls = re.findall(url_regex, message.content)
+                log_count = 0
+                for url in urls:
+                    spoiler = await spoiler_check(message.content)
+                    url = url[0] + url[1] + url[2] + url[3]
+                    for r in replace:
+                        if r in url and "fxtwitter.com" not in url and "vxtwitter.com" not in url:
+                            url = url.replace(r, "fxtwitter.com")
+                            if spoiler:
+                                url = "||" + url + "||"
+                            new_content += f"{url}\n"
+                            log_count += 1
 
-            urls = re.findall(url_regex, message.content)
-            log_count = 0
-            for url in urls:
-                url = url[0] + url[1] + url[2] + url[3]
-                for r in replace:
-                    if r in url and "fxtwitter.com" not in url and "vxtwitter.com" not in url:
-                        url = url.replace(r, "fxtwitter.com")
-                        new_content += f"{url}\n"
-                        log_count += 1
-
-            if len(urls) > 0:
-                await self.log.update(message.guild.id, message.author.id, log_count)
-                prefix = f"Fixing Twitter link for {message.author.display_name}\n"
-                new_content = prefix + new_content
-                await message.reply(new_content, mention_author=False)
+                if len(urls) > 0:
+                    await self.log.update(message.guild.id, message.author.id, log_count, )
+                    prefix = f"Fixing Twitter for {message.author.display_name}\n"
+                    if spoiler:
+                        prefix = f"Fixing Twitter for {message.author.display_name} with hidden content\n"
+                    new_content = prefix + new_content
+                    await message.reply(new_content, mention_author=False)
+                    return
 
     @commands.is_owner()
     @commands.hybrid_command(name="twitstatus", with_app_command=True, description="Toggle Twitter link fixer.")
@@ -86,6 +110,21 @@ class TwitFix(commands.Cog):
         embed.add_field(name="Top Users Fixed", value="\n".join([f"{v} : User ID {k}" for k, v in list(user_stats.items())[:5]]), inline=False)
         await ctx.send(embed=embed)
 
+    @commands.hybrid_command(name="tfremind", with_app_command=True, description="Toggle ignoring reply reminders.")
+    async def tfremind(self, ctx):
+        cur = await self.log.get_ignored(ctx.author.id)
+        if cur is False:
+            await self.log.add_ignored(ctx.author.id)
+            await ctx.author.send("You will no longer receive reply reminders.")
+        else:
+            await self.log.rem_ignored(ctx.author.id)
+            await ctx.author.send("You will now receive reply reminders.")
+async def spoiler_check(message):
+    split = message.split("||")
+    if len(split) >= 2:
+        return True
+    return False
+
 class TwitLog:
     def __init__(self):
         self.filepath = "twitlog.json"
@@ -120,6 +159,18 @@ class TwitLog:
         async with self.lock:
             self.data["links_fixed"] += entryNum
 
+    async def add_ignored(self, userID):
+        userID = str(userID)
+        async with self.lock:
+            if userID not in self.data["ignored"]:
+                self.data["ignored"][userID] = True
+
+    async def rem_ignored(self, userID):
+        userID = str(userID)
+        async with self.lock:
+            if userID in self.data["ignored"]:
+                self.data["ignored"].pop(userID, None)
+
     async def get_stats(self):
         async with self.lock:
             sorted_servers = {k: v for k, v in sorted(self.data["servers"].items(), key=lambda item: item[1], reverse=True)}
@@ -140,6 +191,13 @@ class TwitLog:
             if userID not in self.data["users"]:
                 return 0
             return self.data["users"][userID]
+
+    async def get_ignored(self, userID):
+        userID = str(userID)
+        async with self.lock:
+            if userID not in self.data["ignored"]:
+                return False
+            return True
 
     async def update(self, serverID, userID, entryNum):
         await self.add_to_server(serverID, entryNum)
